8ded0374319b715312ed5e4c338724b11587b320
John Phillips <development@johnphillips.dev>
2023-08-21 17:31:16 +0100

    initial styling setup for typography

--- galleria-slideshow-site/app/layout.js
+++ galleria-slideshow-site/app/layout.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import './globals.css';
 
 export const metadata = {
@@ -8,7 +9,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <header>
+          <div className='flex justify-between py-6 container'>
+            <Image src='/assets/shared/logo.svg' width={113} height={32} alt='Gallery Logo' />
+            <button className='link-1'>Start Slideshow</button>
+          </div>
+        </header>
+        {children}
+        <footer></footer>
+      </body>
     </html>
   );
 }
--- galleria-slideshow-site/app/page.js
+++ galleria-slideshow-site/app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <div className='container'>
+        <h1 className='heading-1'>Hello World</h1>
+        <h1 className='heading-2'>Hello World</h1>
+        <h1 className='heading-3'>Hello World</h1>
+        <h1 className='link-1'>Hello World</h1>
+        <h1 className='link-2'>Hello World</h1>
+        <h1 className='parapgraph'>Hello World</h1>
+      </div>
     </main>
   );
 }
