9276d5a36938897a8175d0f22f3017d37e2acf1a
John Phillips <development@johnphillips.dev>
2023-08-21 22:51:54 +0100

    fetching data for card layout

--- galleria-slideshow-site/app/page.jsx
+++ galleria-slideshow-site/app/page.jsx
@@ -1,10 +1,36 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import ImageCard from '@/components/ImageCard';
 
 export default function Home() {
+  // Setting data states to handle rendering.
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  // Fetch data from api src using provided data.
+  useEffect(() => {
+    fetch('/api/data')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+  }, []);
+
+  // Loop over data and create card logic.
+  const imageCards = data?.map((artworkData, index) => {
+    const {
+      images: { thumbnail, gallery },
+    } = artworkData;
+
+    return <ImageCard key={index} thumbnail={thumbnail} />;
+  });
+
   return (
-    <main>
+    <main className='py-[24px]'>
       <div className='container'>
-        <ImageCard />
+        <div className='grid gap-[24px]'>{imageCards}</div>
       </div>
     </main>
   );
