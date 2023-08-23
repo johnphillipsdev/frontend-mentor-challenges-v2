626a8f9d728eab1b9b5dd83e2bbf8bf11d071b12
John Phillips <development@johnphillips.dev>
2023-08-23 15:11:24 +0100

    tweaked logo size

--- galleria-slideshow-site/app/layout.jsx
+++ galleria-slideshow-site/app/layout.jsx
@@ -12,8 +12,8 @@ export default function RootLayout({ children }) {
       <body className='relative'>
         <header className='sticky top-0 z-50 bg-white'>
           <div className='flex justify-between py-6 container border-b border-light-gray'>
-            <figure>
-              <Image src='/assets/shared/logo.svg' width={113} height={32} alt='Gallery Logo' priority />
+            <figure className='relative w-[113px] md:w-[170px]'>
+              <Image src='/assets/shared/logo.svg' width={113} height={32} className='w-full h-full' alt='Gallery Logo' priority />
             </figure>
             <button className='link-1'>Start Slideshow</button>
           </div>
