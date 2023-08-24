4eac127c5d47cdc616f9fbd03f1200a40667f5b1
John Phillips <development@johnphillips.dev>
2023-08-24 11:48:56 +0100

    image added to loader

--- galleria-slideshow-site/components/LoadingScreen/Loader.jsx
+++ galleria-slideshow-site/components/LoadingScreen/Loader.jsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import Spinner from './Spinner';
 
 export default function Loader() {
@@ -5,6 +6,7 @@ export default function Loader() {
     <div className='absolute z-50 w-full h-screen bg-white flex flex-col justify-center place-items-center p-10'>
       <Spinner />
       <p className='link-1 cursor-auto mt-auto'>Loading Gallery...</p>
+      <Image className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-0' src='/assets/girl-with-pearl-earring/thumbnail.jpg' alt='Gallary Picture Title' priority width={310} height={400} />
     </div>
   );
 }
