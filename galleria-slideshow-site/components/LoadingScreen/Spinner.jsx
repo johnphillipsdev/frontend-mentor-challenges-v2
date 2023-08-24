f65497ede8adfe4112f1616cfc1bcd6dd6e9f8b6
John Phillips <development@johnphillips.dev>
2023-08-24 15:50:52 +0100

    classname fixed

--- galleria-slideshow-site/components/LoadingScreen/Spinner.jsx
+++ galleria-slideshow-site/components/LoadingScreen/Spinner.jsx
@@ -1,12 +1,12 @@
 export default function Spinner() {
   return (
-    <span id='loading-spinner' class='dot-animation mt-auto relative w-10 h-10 z-10'>
-      <span class='chase-dot'></span>
-      <span class='chase-dot'></span>
-      <span class='chase-dot'></span>
-      <span class='chase-dot'></span>
-      <span class='chase-dot'></span>
-      <span class='chase-dot'></span>
+    <span id='loading-spinner' className='dot-animation mt-auto relative w-10 h-10 z-10'>
+      <span className='chase-dot'></span>
+      <span className='chase-dot'></span>
+      <span className='chase-dot'></span>
+      <span className='chase-dot'></span>
+      <span className='chase-dot'></span>
+      <span className='chase-dot'></span>
     </span>
   );
 }
